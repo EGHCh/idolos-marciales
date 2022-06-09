4c6edda8da01f872f62e90dc81b183f1c3b0f47e
Esteban Hormazabal <esteban.hormazabal.ch@gmail.com>
2022-06-09 02:19:26 -0400

    Desafio completo + carrito

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/peleadores">Peleadores</router-link> |
+      <router-link to="/buscador">Buscador</router-link>
+      <img src="https://i.pinimg.com/originals/bd/14/9c/bd149c5c05185de274f040d809f93354.png" alt="" style="width: 30px; height: 30px">
+      {{ cantidadProductosEnElCarrito }}
+    </nav>
+
+    <router-view @agregar="agregar" />
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    cantidadProductosEnElCarrito: 0
+  }),
+  mounted() {
+    if(localStorage.cantidadProductosEnElCarrito) this.cantidadProductosEnElCarrito = localStorage.cantidadProductosEnElCarrito;
+  },
+  watch:{
+    cantidadProductosEnElCarrito(nuevoProducto) {
+      localStorage.cantidadProductosEnElCarrito = nuevoProducto;
+    }
+  },
+  methods: {
+    agregar(){
+      this.cantidadProductosEnElCarrito++
+    }
+  }
+}
+</script>
